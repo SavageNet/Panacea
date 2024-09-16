a66547ceab36c916c9c9262fe127c16b58e52343
andrey.kiselev <andrey.kiselev@ext.it-element29.tech>
2024-09-16 04:56:44 +0300

    delete spaces

--- Graph/DenoiseForm.cs
+++ Graph/DenoiseForm.cs
@@ -46,17 +46,13 @@ namespace Graph
             this.numericUpDownIncrement.Value = this.numericUpDownIncrement.Increment;
             this.numericUpDownSmoothValue.Increment = this.numericUpDownIncrement.Value;
             this.numericUpDownSmoothValue.Value = this.numericUpDownIncrement.Value;
-
             var threshold = (double)numericUpDownSmoothValue.Value;
-
             this.chart.ChartAreas["Result"].AxisX.LabelStyle.Format = "0";
-            this.chart.ChartAreas["PSD"].AxisX.LabelStyle.Format = "0.00";
-
+            this.chart.ChartAreas["PSD"].AxisX.LabelStyle.Format = "0.00"
             this.checkBoxHideOriginal.CheckedChanged += (sender, args) =>
             {
                 this.chart.Series["Original"].Color = this.checkBoxHideOriginal.Checked ? Color.Transparent : Color.DeepSkyBlue;
             };
-
             this.buttonLoad.Click += (sender, args) =>
             {
                 if (dialog.ShowDialog() == DialogResult.OK)
